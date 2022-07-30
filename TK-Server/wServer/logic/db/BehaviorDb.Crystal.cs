0f8ce49728c42740892602349079f7498bae23a7
rusjaell <Survivalistrivalist@gmail.com>
2022-07-30 19:14:31 +0100

    Some Map Moves

--- TK-Server/wServer/logic/db/BehaviorDb.Crystal.cs
+++ TK-Server/wServer/logic/db/BehaviorDb.Crystal.cs
@@ -174,8 +174,8 @@ namespace wServer.logic
                     new TimedTransition(5000, "Whoa_nelly")
                     ),
                 new State("Whoa_nelly",
+                    new HpLessTransition(0.1, "dead"),
                     new Prioritize(
-                        new HpLessTransition(0.1, "dead"),
                         new StayCloseToSpawn(0.6, range: 7),
                         new Wander(0.6)
                         ),

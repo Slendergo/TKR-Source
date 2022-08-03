3c8baa69c20f569d35580e63fcfef04d1aa4e95b
Slendergo <Survivalistrivalist@gmail.com>
2022-08-03 20:51:05 +0100

    Simpler Downloader
    Much simpler downloader but at the cost of more bandwidth usage in the long run
    Mabye we can package the air client along side the install and only update swf part

--- TK-Server/Launcher/Pages/PlayPage.xaml.cs
+++ TK-Server/Launcher/Pages/PlayPage.xaml.cs
@@ -1,139 +1,117 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Net.Http;
-using System.Text;
-using System.Text.Json;
+using System.Threading;
 using System.Threading.Tasks;
-using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Launcher.Components
 {
-    public class JsonMap
-    {
-        public string Version { get; set; }
-        public List<Record> Records { get; set; } = new List<Record>();
-    }
-
-    public class Record
-    {
-        public string Path { get; set; }
-        public string CheckSum { get; set; }
-    }
-
-
-    /// <summary>
-    /// Interaction logic for PlayPage.xaml
-    /// </summary>
     public partial class PlayPage : Page
     {
         public PlayPage()
         {
             InitializeComponent();
 
-            CheckForUpdates();
+            Download();
         }
 
-        public JsonMap LauncherJsonMap { get; private set; }
-        public JsonMap ServerLauncherJsonMap { get; private set; }
+        public string Version = "1.0";
 
-        public void CheckForUpdates()
+        private static HttpClient HttpClient = new HttpClient();
+
+        public async void Download()
         {
-            Task.Factory.StartNew(() =>
-            {
-                FetchChecksum();
-            }).ContinueWith(_ =>
-            {
-                Trace.WriteLine("Checking for changes");
+            var progress = new Progress<(string, double)>();
 
-                var fileToUpdate = new List<string>();
-                var fileToRemove = new List<string>();
+            progress.ProgressChanged += Progress_ProgressChanged;
 
-                // check the current files to the client for removals
-                foreach (var file in LauncherJsonMap.Records)
+            var cancellationToken = new CancellationTokenSource();
+            await DownloadFileAsync("http://www.dotpdn.com/files/Paint.NET.3.5.11.Install.zip", progress, cancellationToken.Token);
+        }
+
+        private void Progress_ProgressChanged(object? sender, (string text, double value) e)
+        {
+            ProgressBar.Value = e.value;
+            ProgressText.Text = $"Downloading: {e.text}";
+            if(ProgressBar.Value == ProgressBar.Maximum)
+                ProgressText.Text = "Downloaded";
+        }
+        
+        public async Task DownloadFileAsync(string url, IProgress<(string, double)> progress, CancellationToken token)
+        {
+            var response = await HttpClient.GetAsync(url, HttpCompletionOption.ResponseHeadersRead, token);
+
+            if (!response.IsSuccessStatusCode)
+                throw new Exception(string.Format("The request returned with HTTP status code {0}", response.StatusCode));
+
+            var total = response.Content.Headers.ContentLength.HasValue ? response.Content.Headers.ContentLength.Value : -1L;
+            var canReportProgress = total != -1 && progress != null;
+
+            using (var stream = await response.Content.ReadAsStreamAsync())
+            {
+                var totalRead = 0L;
+                var buffer = new byte[4096];
+                var isMoreToRead = true;
+
+                do
                 {
-                    var clientFile = ServerLauncherJsonMap.Records.FirstOrDefault(_ => file.Path == _.Path);
-                    if (clientFile == null)
+                    token.ThrowIfCancellationRequested();
+
+                    var read = await stream.ReadAsync(buffer, 0, buffer.Length, token);
+
+                    if (read == 0)
                     {
-                        Trace.WriteLine($"Remove {file.CheckSum}");
-                        fileToRemove.Add(file.Path);
-                        continue;
+                        isMoreToRead = false;
                     }
-
-                    // if it exists check the chechsum for changes
-                    if (file.CheckSum != clientFile.CheckSum)
+                    else
                     {
-                        Console.WriteLine($"Update {file.CheckSum} -> {clientFile.CheckSum}");
-                        fileToUpdate.Add(file.CheckSum);
-                        // file has changed
+                        var data = new byte[read];
+                        buffer.ToList().CopyTo(0, data, 0, read);
+
+                        // TODO: put here the code to write the file to disk
+
+                        totalRead += read;
+
+                        if (canReportProgress)
+                        {
+                            progress.Report((SizeSuffix(totalRead) + " / " + SizeSuffix(total), (totalRead * 1d) / (total * 1d) * 100));
+                        }
                     }
-                }
-
-                var fileToAdd = new List<string>();
-                foreach (var file in ServerLauncherJsonMap.Records)
-                {
-                    if (fileToUpdate.Contains(file.Path))
-                        continue;
-                    Trace.WriteLine($"Add {file.CheckSum}");
-                    fileToAdd.Add(file.CheckSum);
-                }
-
-                Task.Factory.StartNew(() =>
-                {
-                    var compressedZip = RequestFiles(fileToAdd);
-
-                    using var f = File.Create("Zip.zip");
-                    f.Write(compressedZip, 0, compressedZip.Length);
-                });
-            });
+                } while (isMoreToRead);
+            }
         }
 
-        public byte[] RequestFiles(List<string> files)
+        static readonly string[] SizeSuffixes = { "b", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB" };
+        static string SizeSuffix(long value, int decimalPlaces = 2)
         {
-            using var client = new HttpClient();
+            if (decimalPlaces < 0)
+                throw new ArgumentOutOfRangeException("decimalPlaces");
 
-            var json = JsonSerializer.Serialize(files);
-            var content = new StringContent(json, Encoding.UTF8, "application/json");
+            if (value < 0)
+                return "-" + SizeSuffix(-value, decimalPlaces);
 
-            var response = client.PostAsync("http://127.0.0.1:1999/launcher/requestChanges", content).GetAwaiter().GetResult();
-            return response.Content.ReadAsByteArrayAsync().GetAwaiter().GetResult();
-        }
+            if (value == 0)
+                return string.Format("{0:n" + decimalPlaces + "} bytes", 0);
 
-        public void FetchChecksum()
-        {
-            using var client = new HttpClient();
+            // mag is 0 for bytes, 1 for KB, 2, for MB, etc.
+            int mag = (int)Math.Log(value, 1024);
 
-            var values = new Dictionary<string, string>();
+            // 1L << (mag * 10) == 2 ^ (10 * mag) 
+            // [i.e. the number of bytes in the unit corresponding to mag]
+            decimal adjustedSize = (decimal)value / (1L << (mag * 10));
 
-            var content = new FormUrlEncodedContent(values);
-
-            var response = client.PostAsync("http://127.0.0.1:1999/launcher/checkSum", content).GetAwaiter().GetResult();
-
-            var responseString = response.Content.ReadAsStringAsync().GetAwaiter().GetResult();
-
-            ServerLauncherJsonMap = JsonSerializer.Deserialize<JsonMap>(responseString);
-
-            if (!File.Exists("CheckSum.json"))
+            // make adjustment when the value is large enough that
+            // it would round up to 1000 or more
+            if (Math.Round(adjustedSize, decimalPlaces) >= 1000)
             {
-                using var file = File.CreateText("CheckSum.json");
-                file.WriteLine(responseString);
-                LauncherJsonMap = JsonSerializer.Deserialize<JsonMap>(responseString);
+                mag += 1;
+                adjustedSize /= 1024;
             }
-            else
-            {
-                var json = File.ReadAllText("CheckSum.json");
-                LauncherJsonMap = JsonSerializer.Deserialize<JsonMap>(json);
-            }
+
+            return string.Format("{0:n" + decimalPlaces + "} {1}", adjustedSize, SizeSuffixes[mag]);
         }
     }
 }

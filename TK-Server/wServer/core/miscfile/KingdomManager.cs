49d2a3cf2d6de5217c9d2841c849b5ce57e1da77
Aaron <aaron34gtt@gmail.com>
2022-08-03 17:32:25 +1000

    Remove hermit

--- TK-Server/wServer/core/miscfile/KingdomManager.cs
+++ TK-Server/wServer/core/miscfile/KingdomManager.cs
@@ -437,7 +437,7 @@ namespace wServer.core
             Tuple.Create("Lord of the Lost Lands", (ISetPiece) null),
             Tuple.Create("Ghost Ship", (ISetPiece) new GhostShip()),
             Tuple.Create("Grand Sphinx", (ISetPiece) new Sphinx()),
-            Tuple.Create("Hermit God", (ISetPiece) new Hermit()),
+            //Tuple.Create("Hermit God", (ISetPiece) new Hermit()),
             Tuple.Create("Skull Shrine", (ISetPiece) new SkullShrine()),
             Tuple.Create("Lucky Ent God", (ISetPiece) null),
             Tuple.Create("Lucky Djinn", (ISetPiece) null),

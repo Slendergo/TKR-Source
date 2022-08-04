0f666b832348e975f69a3739266b5110d782986b
Aaron <aaron34gtt@gmail.com>
2022-08-05 01:10:42 +1000

    Change apostrophe

--- TK-Server/wServer/logic/db/BehaviorDb.JuliusCaesar.cs
+++ TK-Server/wServer/logic/db/BehaviorDb.JuliusCaesar.cs
@@ -150,7 +150,7 @@ namespace wServer.logic
                     new ItemLoot("Spear of Thiram", 0.0014)
                 ),
               new Threshold(0.03,
-                    new ItemLoot("Gladiator’s Visage", 0.0014)
+                    new ItemLoot("Gladiator's Visage", 0.0014)
                 ),
                     new Threshold(0.001,
                         new ItemLoot("Crafting Material 1", 0.05),

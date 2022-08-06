54588335f88dc8edd98e60a5f3c4541141082953
Orb <simmermakerethan@gmail.com>
2022-08-05 23:40:17 -0400

    r

--- TK-Server/wServer/logic/db/BehaviorDb.Elementals.cs
+++ TK-Server/wServer/logic/db/BehaviorDb.Elementals.cs
@@ -325,13 +325,13 @@ namespace wServer.logic
                     ),
                 new State("Orbit",
                     new Prioritize(
-                        new Orbit(5, 8, 20, "Fire Elemental", 1, 1, speedVariance: 1)),
+                        new Orbit(5, 8, 20, "Fire Elemental", 1, 1, speedVariance: 0.2, radiusVariance: 0.2)),
                     new Shoot(20, 12, projectileIndex: 0, coolDown: 500),
                     new EntityNotExistsTransition("Fire Elemental", 20, "Die")
                     ),
                 new State("Orbit 2",
                     new Prioritize(
-                        new Orbit(5, 5, 20, "Fire Elemental", 1, 1, speedVariance: 1)),
+                        new Orbit(5, 5, 20, "Fire Elemental", 1, 1, speedVariance: 0.2, radiusVariance: 0.2)),
                     new Shoot(20, 12, projectileIndex: 0, coolDown: 500),
                     new EntityNotExistsTransition("Fire Elemental", 20, "Die")
                     ), 

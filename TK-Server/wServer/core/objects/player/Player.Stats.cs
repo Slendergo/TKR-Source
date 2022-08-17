c272df89ca104c445734d1dbe8adf7a530338ac4
rusjaell <Survivalistrivalist@gmail.com>
2022-08-17 10:58:14 +0100

    Update Player.Stats.cs

--- TK-Server/wServer/core/objects/player/Player.Stats.cs
+++ TK-Server/wServer/core/objects/player/Player.Stats.cs
@@ -14,14 +14,6 @@ namespace wServer.core.objects
         public bool MaxedSpd { get => _maxedSpd.GetValue(); set => _maxedSpd.SetValue(value); }
         public bool MaxedVit { get => _maxedVit.GetValue(); set => _maxedVit.SetValue(value); }
         public bool MaxedWis { get => _maxedWis.GetValue(); set => _maxedWis.SetValue(value); }
-        public bool SuperMaxedAtt { get => _superMaxedAtt.GetValue(); set => _superMaxedAtt.SetValue(value); }
-        public bool SuperMaxedDef { get => _superMaxedDef.GetValue(); set => _superMaxedDef.SetValue(value); }
-        public bool SuperMaxedDex { get => _superMaxedDex.GetValue(); set => _superMaxedDex.SetValue(value); }
-        public bool SuperMaxedLife { get => _superMaxedLife.GetValue(); set => _superMaxedLife.SetValue(value); }
-        public bool SuperMaxedMana { get => _superMaxedMana.GetValue(); set => _superMaxedMana.SetValue(value); }
-        public bool SuperMaxedSpd { get => _superMaxedSpd.GetValue(); set => _superMaxedSpd.SetValue(value); }
-        public bool SuperMaxedVit { get => _superMaxedVit.GetValue(); set => _superMaxedVit.SetValue(value); }
-        public bool SuperMaxedWis { get => _superMaxedWis.GetValue(); set => _superMaxedWis.SetValue(value); }
 
         private SV<bool> _maxedAtt;
         private SV<bool> _maxedDef;
@@ -31,14 +23,6 @@ namespace wServer.core.objects
         private SV<bool> _maxedSpd;
         private SV<bool> _maxedVit;
         private SV<bool> _maxedWis;
-        private SV<bool> _superMaxedAtt;
-        private SV<bool> _superMaxedDef;
-        private SV<bool> _superMaxedDex;
-        private SV<bool> _superMaxedLife;
-        private SV<bool> _superMaxedMana;
-        private SV<bool> _superMaxedSpd;
-        private SV<bool> _superMaxedVit;
-        private SV<bool> _superMaxedWis;
 
         public StatsManager Stats;
     }

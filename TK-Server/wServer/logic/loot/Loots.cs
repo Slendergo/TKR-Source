da55be3f771faf469da459a2221043bd0808323b
Slendergo <Survivalistrivalist@gmail.com>
2022-08-25 21:43:17 +0100

    Dusts

--- TK-Server/wServer/logic/loot/Loots.cs
+++ TK-Server/wServer/logic/loot/Loots.cs
@@ -115,24 +115,24 @@ namespace wServer.logic.loot
 
             if (enemy.Legendary)
             {
-                list.Add(new LootDef("Potion Dust", 0.25, 0.001));
-                list.Add(new LootDef("Item Dust", 0.025, 0.001));
-                list.Add(new LootDef("Miscellaneous Dust", 0.02, 0.001));
-                list.Add(new LootDef("Special Dust", 0.003, 0.001));
+                list.Add(new LootDef("Potion Dust", 0.08, 0.001)); // 8%
+                list.Add(new LootDef("Item Dust", 0.12, 0.001)); // 12%
+                list.Add(new LootDef("Miscellaneous Dust", 0.05, 0.001)); // 5% 
+                list.Add(new LootDef("Special Dust", 0.03, 0.001)); // 3%
             }
             else if (enemy.Epic)
             {
-                list.Add(new LootDef("Potion Dust", 0.25, 0.001));
-                list.Add(new LootDef("Item Dust", 0.015, 0.001));
-                list.Add(new LootDef("Miscellaneous Dust", 0.01, 0.001));
-                list.Add(new LootDef("Special Dust", 0.002, 0.001));
+                list.Add(new LootDef("Potion Dust", 0.05, 0.001)); // 5%
+                list.Add(new LootDef("Item Dust", 0.08, 0.001)); //8%
+                list.Add(new LootDef("Miscellaneous Dust", 0.02, 0.001)); //2%
+                list.Add(new LootDef("Special Dust", 0.01, 0.001)); // 1%
             }
             else if (enemy.Rare)
             {
-                list.Add(new LootDef("Potion Dust", 0.5, 0.001));
-                list.Add(new LootDef("Item Dust", 0.01, 0.001));
-                list.Add(new LootDef("Miscellaneous Dust", 0.005, 0.001));
-                list.Add(new LootDef("Special Dust", 0.001, 0.001));
+                list.Add(new LootDef("Potion Dust", 0.03, 0.001));//3%
+                list.Add(new LootDef("Item Dust", 0.05, 0.001)); //5%
+                list.Add(new LootDef("Miscellaneous Dust", 0.01, 0.001)); //1%
+                list.Add(new LootDef("Special Dust", 0.005, 0.001)); //0.5%
             }
 
             return list;

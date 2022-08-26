ef2fec81ae70424c05e916c949ea7fb586534b83
Slendergo <Survivalistrivalist@gmail.com>
2022-08-27 00:34:05 +0100

    New Version

--- TK-Server/wServer/core/commands/admin/Command.Gift.cs
+++ TK-Server/wServer/core/commands/admin/Command.Gift.cs
@@ -1,4 +1,5 @@
 ﻿using common;
+using common.resources;
 using System;
 using System.Linq;
 using wServer.core.objects;
@@ -72,14 +73,9 @@ namespace wServer.core.commands
             {
                 var gameData = player.GameServer.Resources.GameData;
 
-
                 // allow both DisplayId and Id for query
-                if (!gameData.DisplayIdToObjectType.TryGetValue(itemName, out ushort objType))
-                {
-                    if (!gameData.IdToObjectType.TryGetValue(itemName, out objType))
-                        player.SendError("Unknown item type!");
+                if (!gameData.IdToObjectType.TryGetValue(itemName, out ushort objType))
                     return null;
-                }
 
                 if (!gameData.Items.ContainsKey(objType))
                 {

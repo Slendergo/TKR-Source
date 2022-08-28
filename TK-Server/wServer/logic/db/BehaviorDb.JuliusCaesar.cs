8529e2dca98e350c9e7cd542805775728e50b0dd
Slendergo <Survivalistrivalist@gmail.com>
2022-08-28 17:02:01 +0100

    Update BehaviorDb.JuliusCaesar.cs

--- TK-Server/wServer/logic/db/BehaviorDb.JuliusCaesar.cs
+++ TK-Server/wServer/logic/db/BehaviorDb.JuliusCaesar.cs
@@ -10,7 +10,7 @@ namespace wServer.logic
         private _ JuliusCaesar = () => Behav()
         .Init("Julius Caesar",
             new State(
-                new ScaleHP2(20),
+                new ScaleHP2(35),
                 new State("idle",
                     new ConditionalEffect(ConditionEffectIndex.Invincible, true), //invuln
                     new PlayerWithinTransition(15, "Prepare")

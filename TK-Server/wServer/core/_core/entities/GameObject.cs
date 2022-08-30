10a03452a1fe2b9e49434564a7c7538a2628abd1
rusjaell <Survivalistrivalist@gmail.com>
2022-08-30 11:32:57 +0100

    Stuff

--- TK-Server/wServer/core/_core/entities/GameObject.cs
+++ TK-Server/wServer/core/_core/entities/GameObject.cs
@@ -7,32 +7,59 @@ using wServer.core.worlds;
 
 namespace wServer.core._core.entities
 {
-    public sealed class GameObject
+    public class Momentum
+    {
+        public float Speed;
+        public float Facing;
+        public GameObject GameObject;
+
+        public Momentum(GameObject gameObject)
+        {
+            GameObject = gameObject;
+        }
+
+        public bool Move(float dt)
+        {
+
+            return true;
+        }
+    }
+
+    public abstract class GameObject
     {
         public int Id;
         public int ObjectType;
+        public World World;
         public float X;
         public float Y;
         public float PrevX;
         public float PrevY;
+        public Momentum Momentum;
 
         public IntPoint IPosition => new IntPoint((int)X, (int)Y);
         public Position Position => new Position(X, Y);
 
-        public GameObject(World world, int objectId, int objectType)
+        public GameObject(int objectId, int objectType, World world)
         {
+            Id = objectId;
+            ObjectType = objectType;
+            World = world;
+
+            Momentum = new Momentum(this);
         }
 
-        public void OnEnterWorld()
+        public void SetPosition(float x, float y)
         {
+            var prevX = X;
+            var prevY = Y;
+            X = x;
+            Y = y;
+            PrevX = prevX;
+            PrevY = prevY;
         }
 
-        public void Update(ref TickTime tickTime)
-        {
-        }
-
-        public void OnLeaveWorld()
-        {
-        }
+        public abstract void OnEnterWorld();
+        public abstract bool Update(ref TickTime tickTime);
+        public abstract void OnLeaveWorld();
     }
 }

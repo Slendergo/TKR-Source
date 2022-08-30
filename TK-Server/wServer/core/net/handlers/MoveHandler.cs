7d3e2c8849e2667cf7c48f832e6a1458499dc12e
Slendergo <Survivalistrivalist@gmail.com>
2022-08-30 18:55:44 +0100

    Update MoveHandler.cs

--- TK-Server/wServer/core/net/handlers/MoveHandler.cs
+++ TK-Server/wServer/core/net/handlers/MoveHandler.cs
@@ -39,7 +39,7 @@ namespace wServer.core.net.handlers
                 if (player.Quest != null && player.Dist(new Position(newX, newY)) > 50 && player.Quest.Dist(new Position(newX, newY)) < 0.25)
                 {
                     StaticLogger.Instance.Warn($"{player.Name} was caught teleporting directly to a quest, uh oh");
-                    player.Client.Disconnect("Out of map bounds");
+                    player.Client.Disconnect("Unexpected Error Occured");
                     return;
                 }
 

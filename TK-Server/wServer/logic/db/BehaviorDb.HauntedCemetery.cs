158a9fd6a6cd1001a3b4637d46b6c3ba61099101
Orb <simmermakerethan@gmail.com>
2022-08-31 15:53:12 -0400

    fixes

--- TK-Server/wServer/logic/db/BehaviorDb.HauntedCemetery.cs
+++ TK-Server/wServer/logic/db/BehaviorDb.HauntedCemetery.cs
@@ -2479,9 +2479,9 @@ namespace wServer.logic
                 //new ItemLoot("Amulet of Resurrection", 0.01),
                 //new ItemLoot("Tamed Werewolf Cub Egg", 0.03),
                 new ItemLoot("Wine Cellar Incantation", 0.01),
-                new ItemLoot("Dark Skeleton's Shield", 0.0008),
+                new ItemLoot("Dark Skeleton's Shield", 0.00025),
                 new ItemLoot("Plague Poison", 0.008),
-                new ItemLoot("Resurrected Warrior's Armor", 0.008)
+                new ItemLoot("Resurrected Warrior's Armor", 0.001)
                 )
             )
         .Init("Halloween Zombie Spawner",
